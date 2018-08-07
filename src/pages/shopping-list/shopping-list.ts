39b42a71fc95ac93953d9a3730296951ce3deae0
CHOIJAE HOON <hoonja@CHOIJAEui-iMac.local>
2018-08-07 18:15:12 +0900

    first commit
    ~ Lecture 159

---
+++ src/pages/shopping-list/shopping-list.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { IonicPage, PopoverController } from 'ionic-angular';
+import { NgForm } from '@angular/forms';
+import { ShoppingListService } from '../../services/shopping-list';
+import { Ingredient } from '../../models/ingredient';
+import { SLOptionsPage } from './sl-options/sl-options';
+
+@IonicPage()
+@Component({
+  selector: 'page-shopping-list',
+  templateUrl: 'shopping-list.html',
+})
+export class ShoppingListPage {
+  listItems: Ingredient[];
+
+  constructor(
+    private slService: ShoppingListService,
+    private popoverCtrl: PopoverController
+  ) {}
+
+  ionViewWillEnter() {
+    this.loadItems();
+  }
+
+  onAddItem(form: NgForm) {
+    this.slService.addItem(form.value.ingredientName, form.value.amount);
+    form.reset();
+    this.loadItems();
+  }
+
+  onCheckItem(index: number) {
+    this.slService.removeItem(index);
+    this.loadItems();
+  }
+
+  onShowOptions(event: MouseEvent) {
+    const popover = this.popoverCtrl.create(SLOptionsPage);
+    popover.present({ev: event});
+  }
+
+  private loadItems() {
+    this.listItems = this.slService.getItems();
+  }
+}
